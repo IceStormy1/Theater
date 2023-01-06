a6493c47cacf4f9c1e29a7228e6cb002f04859b1
IceStormy1 <mishytka-02@mail.ru>
2023-01-06 20:47:41 +0300

    Add EnumExtensions

---
+++ Theatre/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Theater.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumDisplayName(this Enum property)
+        {
+            var enumDisplayName = property.GetType()
+                .GetMember(property.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName();
+
+            return enumDisplayName;
+        }
+    }
+}
