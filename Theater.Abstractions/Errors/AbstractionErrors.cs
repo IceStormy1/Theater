a16fb5b2a9ccef5f87a33484447517a6d54475eb
Tolmachev Mikhail <55889141+IceStormy1@users.noreply.github.com>
2023-06-05 10:07:22 +0300

    Add abstraction errors

---
+++ Theater.Abstractions/Errors/AbstractionErrors.cs
@@ -0,0 +1,12 @@
+﻿using Theater.Common;
+
+namespace Theater.Abstractions.Errors;
+
+public static class AbstractionErrors
+{
+    /// <summary>
+    /// Дата для указанной пьесы уже существует
+    /// </summary>
+    public static WriteResult InternalError =>
+        WriteResult.FromError(ErrorModel.NotFound("internal-error", "Произошла непредвиденная ошибка"));
+}
