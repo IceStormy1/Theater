90cb4a207ea2a7a6d467c641323f337a66fe0535
Tolmachev Mikhail <55889141+IceStormy1@users.noreply.github.com>
2023-11-17 12:18:00 +0300

    Add Redis to projects

---
+++ Theater.Core/Caches/ConnectionsCache.cs
@@ -0,0 +1,75 @@
+﻿using StackExchange.Redis.Extensions.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Theater.Abstractions.Caches;
+
+namespace Theater.Core.Caches;
+
+public sealed class ConnectionsCache : IConnectionsCache
+{
+    private const string PrefixKey = "rooms_";
+
+    private readonly IRedisClient _redis;
+
+    public ConnectionsCache(IRedisClient redis) 
+        => _redis = redis;
+
+    public async Task SetConnection(Guid userId, string connectionId)
+    {
+        var connections = await GetConnections(userId);
+        if (connections.Add(connectionId))
+        {
+            await _redis.Db0.AddAsync(
+                key: GetChatConnectionsCacheKey(userId),
+                value: connections);
+        }
+    }
+
+    public async Task<HashSet<string>> GetConnections(Guid userId)
+    {
+        var connections = await _redis.Db0.GetAsync<HashSet<string>>(
+            key: GetChatConnectionsCacheKey(userId));
+
+        return connections ?? new HashSet<string>();
+    }
+
+    public async Task RemoveConnection(Guid userId, string connectionId)
+    {
+        var connections = await GetConnections(userId);
+        if (connections.Remove(connectionId))
+        {
+            var key = GetChatConnectionsCacheKey(userId);
+            if (connections.Any())
+                await _redis.Db0.AddAsync(key: key, value: connections);
+            else
+                await _redis.Db0.RemoveAsync(key);
+        }
+    }
+
+    public async Task<IDictionary<Guid, HashSet<string>>> GetUserConnectionsByIds(IEnumerable<Guid> userIds)
+    {
+        var dict = await _redis.Db0.GetAllAsync<HashSet<string>>(
+            new HashSet<string>(userIds.Select(GetChatConnectionsCacheKey)));
+
+        return dict.ToDictionary(x => GetIdFromKey(x.Key), y => y.Value);
+    }
+
+    public async Task ClearConnections(Guid userId)
+    {
+        var key = GetChatConnectionsCacheKey(userId);
+        await _redis.Db0.RemoveAsync(key);
+    }
+
+    private static string GetChatConnectionsCacheKey(Guid userId)
+    {
+        var tt = PrefixKey + userId;
+        return PrefixKey + userId;
+    }
+
+    private static Guid GetIdFromKey(string key)
+    {
+        return Guid.Parse(key[PrefixKey.Length..]);
+    }
+}
--- Theatre/Controllers/TestController.cs
+++ Theatre/Controllers/TestController.cs
@@ -1,33 +1,42 @@
-﻿using MassTransit;
+﻿using AutoMapper;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Threading.Tasks;
+using Theater.Abstractions.Caches;
 using Theater.Contracts.Rabbit;
 using Theater.Contracts.UserAccount;
+using Theater.Controllers.Base;
 
 namespace Theater.Controllers;
 
 [SwaggerTag("Тестовые методы")]
 [ApiController]
 [Route("[controller]")]
-public sealed class TestController : ControllerBase
+public sealed class TestController : BaseController
 {
     private readonly IPublishEndpoint _messageBus;
     private readonly ILogger<TestController> _logger;
+    private readonly IConnectionsCache _connectionsCache;
 
-    public TestController(IPublishEndpoint messageBus, ILogger<TestController> logger)
+    public TestController(
+        IPublishEndpoint messageBus,
+        ILogger<TestController> logger,
+        IConnectionsCache connectionsCache,
+        IMapper mapper) : base(mapper)
     {
         _messageBus = messageBus;
         _logger = logger;
+        _connectionsCache = connectionsCache;
     }
 
     /// <summary>
     /// Отправляет сообщение в тестовую очередь
     /// </summary>
-    [HttpPost]
+    [HttpPost("rabbit")]
     [ProducesResponseType(typeof(UserModel), StatusCodes.Status200OK)]
     public async Task CreateReview()
     {
@@ -36,4 +45,20 @@ public sealed class TestController : ControllerBase
         await _messageBus.Publish(new TestRabbitModel { Id = messageId });
         _logger.LogInformation("Отправлено сообщение с идентификатором {MessageId}", messageId);
     }
+
+    /// <summary>
+    /// Добавляет соединение для залогиненного пользователя в Redis
+    /// </summary>
+    [HttpPost("redis/connections/{connectionId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public Task AddConnection(Guid connectionId)
+        => _connectionsCache.SetConnection(UserId!.Value, connectionId.ToString());
+
+    /// <summary>
+    /// Получает соединения для залогиненного пользователя в Redis
+    /// </summary>
+    [HttpGet("redis/connections")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public Task GetConnections()
+        => _connectionsCache.GetConnections(UserId!.Value);
 }
